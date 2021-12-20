d034b5375677cc51299d4820ea181c911b7b9758
youshyna <70101754+youshyna@users.noreply.github.com>
2021-12-20 16:32:35 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -4,42 +4,51 @@ function btnClick() {
   let result;
 
   num1 = parseInt(document.getElementById('input1').value);
-  console.log(num1);
   num2 = parseInt(document.getElementById('input2').value);
 
   if (selectValue === "+") {
     result = parseInt(num1 + num2);
-    
+
   } else if (selectValue === "-") {
     result = parseInt(num1 - num2);
 
   } else if (selectValue === "*") {
     result = parseInt(num1 * num2);
-    
+
   } else {
     result = parseInt(num1 / num2);
   }
-  
+
   if (isNaN(result)) {
     result = 'incorrect data :(';
   }
 
-  var output = document.getElementById("result");
+  let output = document.getElementById("result");
   output.value = result;
 }
-
 
 function setValue() {
   selectValue = document.getElementById("math").value;
 }
 
-
 function init() {
-  var button = document.getElementById("submit");
+  const button = document.getElementById("submit");
   button.onclick = btnClick;
 }
-
-
 window.onload = init;
+
+
+function invalidMsg(textbox) {
+  if (textbox.value === '') {
+    textbox.setCustomValidity('Please, enter integer number!');
+  } else {
+    textbox.setCustomValidity('');
+  }
+}
+
+function isNumberKey(evt){
+  let charCode = (evt.which) ? evt.which : evt.keyCode;
+  return !((charCode == 46));
+}
 
 
